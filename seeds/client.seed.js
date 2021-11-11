4faa53877aa54cfa774bad932d3a84c5b7b0e915
Frank Friberg <fra.friberg@gmail.com>
2021-11-11 17:38:23 +0100

    updated how client seeding is executed

--- seeds/client.seed.js
+++ seeds/client.seed.js
@@ -1,16 +1,16 @@
-export default [
-  {
-    slug: 'testogtest',
-    name: 'Test & Test',
-    date: '2021-12-09',
-    questions: [
-      {
-        type: 'boolean',
-        label: {
-          nb: 'Trenger du sted å bo?',
-          en: 'Need a place to stay?',
-        },
+export default {
+  slug: 'testogtest',
+  name: 'Test & Test',
+  email: 'test@test.com',
+  password: 'test',
+  date: '2021-12-09',
+  questions: [
+    {
+      type: 'boolean',
+      label: {
+        nb: 'Trenger du sted å bo?',
+        en: 'Need a place to stay?',
       },
-    ],
-  },
-]
+    },
+  ],
+}
--- seeds/index.js
+++ seeds/index.js
@@ -1,4 +1,5 @@
 import { Client, Guest } from '../models'
+import { createNew } from '../controllers/client.controller'
 
 import ClientSeed from './client.seed'
 import GuestSeed from './guest.seed'
@@ -6,7 +7,7 @@ import GuestSeed from './guest.seed'
 export default function seedDatabase() {
   return new Promise((resolve, reject) => {
     Client.deleteMany({}, () => {
-      Client.create(ClientSeed, (err, client) => {
+      createNew(ClientSeed, (err, client) => {
         if (err) return reject(console.error(err))
         Guest.deleteMany({}, () => {
           Guest.create(GuestSeed(client[0]._id), (err, guests) => {
