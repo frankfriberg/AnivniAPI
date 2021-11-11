9902bcc9b5e07673e31c8d05405706a4f330af4f
Frank Friberg <fra.friberg@gmail.com>
2021-11-11 15:57:17 +0100

    Added unauthorized error to middleware

--- helpers/error.js
+++ helpers/error.js
@@ -9,11 +9,13 @@ class GeneralError extends Error {
   getCode() {
     if (this instanceof BadRequest) return 400
     if (this instanceof NotFound) return 404
+    if (this instanceof Unauthorized) return 401
     return 500
   }
 }
 
 class BadRequest extends GeneralError {}
 class NotFound extends GeneralError {}
+class Unauthorized extends GeneralError {}
 
-export { GeneralError, BadRequest, NotFound }
+export { GeneralError, BadRequest, NotFound, Unauthorized }
